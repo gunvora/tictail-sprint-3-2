f8982fd1f8c47ed0bd19e9c7d2458775da7f3b07
Abbe <31575083+AbbeBlubb@users.noreply.github.com>
2018-04-11 14:58:54 +0200

    Routing to product details

--- code/src/components/Productdetail.js
+++ code/src/components/Productdetail.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./productdetail.css"
 
 
 export default class Productdetail extends React.Component {
@@ -6,7 +7,7 @@ export default class Productdetail extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { product: [] }
+    this.state = { product: null }
   }
 
   componentDidMount() {
@@ -18,17 +19,25 @@ export default class Productdetail extends React.Component {
       .then(resp => {
         return resp.json();
       }).then(json => {
-      this.setState({products: json});
+      this.setState({product: json});
     })
   }
 
 render() {
   console.log(this.props)
+  console.log(this.state)
 
+  if (!this.state.product){
+    return null
+  }
   return (
-    <div>Hello</div>
-
+    <div>
+      <div className="product-detail">
+        <img className="product-detail-img" src={this.state.product.images[0].url} />
+        <div className="product-detail-title">{this.state.product.title}</div>
+        <div className="product-detail-price">{this.state.product.price/100} {this.state.product.currency}</div>
+      </div>
+    </div>
   )
-
 }
 }
