6a236e723fffe7b3d4d887d6fcd3f336c108b3f2
Mattias Andersson <mattias.andersson@aftonbladet.se>
2018-04-11 17:01:59 +0200

    Styling gallery

--- code/src/components/Product.js
+++ code/src/components/Product.js
@@ -9,7 +9,9 @@ class Product extends React.Component {
         <img src={this.props.product.images[0].url} className="image" />
           <div className="title">{this.props.product.title}</div>
           <div className="price">{this.props.product.price/100} {this.props.product.currency}</div>
-          <Link to={`/productpage/${this.props.product.id}` }>To product page</Link>
+          <div className="product-button">
+          <button className="button-toproductpage"><Link to={`/productpage/${this.props.product.id}` }>To product page</Link></button>
+          </div>
       </div>
     )
   }
